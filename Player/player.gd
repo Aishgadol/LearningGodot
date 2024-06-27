5c2e40f8b73ef8998b27d4bd5f17d89b78a3edcc
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-06-27 18:58:26 +0300

    more work on learning godot

--- Player/player.gd
+++ Player/player.gd
@@ -6,8 +6,12 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var anim=get_node("AnimatedSprite2D")
 
 
+func _ready():
+	anim.play("BFLY")
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -16,13 +20,19 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"): #and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.scale.x=-1 * direction
+		$AnimatedSprite2D.sprite_frames.set_animation_speed("BFLY",25)
+		#anim.play("BFLY")
 	else:
+		_ready()
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.sprite_frames.set_animation_speed("BFLY",5)
 
 	move_and_slide()
